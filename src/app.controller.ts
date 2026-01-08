8f696ae480b41bf088d9bdd9538d8c33abf032da
Your Name <you@example.com>
2026-01-08 13:10:49 +0900

    배포오류용커밋

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { DateHelper } from './common/utils/date-helper';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { DateHelper } from "./common/utils/date-helper";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
 
-  @Get('health')
-  getHealth() {
-    return {
-      status: 'ok',
-      timestamp: DateHelper.getKoreaTimeISOString(),
-    };
-  }
+	@Get("health")
+	getHealth() {
+		return {
+			status: "정상",
+			timestamp: DateHelper.getKoreaTimeISOString(),
+		};
+	}
 }
-
